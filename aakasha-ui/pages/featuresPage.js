0d7855c8b8f05d7e0bf66b750f72c9f704317746
Ubuntu <ubuntu@ip-172-31-87-33.ec2.internal>
2025-04-17 07:09:58 +0000

    Remove Dump20240412, aakasha-ui, and backend directories from repo

--- aakasha-ui/pages/featuresPage.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const featuresPage = () => {
-  return (
-    <div style={{margin:100  }} className='text-center'>featuresPage</div>
-  )
-}
-
-export default featuresPage
--- aakasha-ui/pages/pricingPage.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const pricingPage = () => {
-  return (
-    <div style={{margin:100  }} className='text-center'>pricingPage</div>
-  )
-}
-
-export default pricingPage
