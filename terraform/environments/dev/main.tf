d20d0e9c27c43f21d5c9c31ef8b96099c32694a3
root <root@ip-172-31-38-241.ec2.internal>
2026-08-17 10:17:37 +0000

    eks module files

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -9,4 +9,18 @@ module "vpc" {
   private_subnets    = var.private_subnets
 }
 
+module "eks" {
+  source = "../../modules/eks"
 
+  cluster_name    = "aakasha-dev"
+  cluster_version = "1.33"
+
+  vpc_id             = module.vpc.vpc_id
+  private_subnet_ids = module.vpc.private_subnet_ids
+
+  node_instance_types = ["t3.medium"]
+
+  desired_nodes = 2
+  min_nodes     = 1
+  max_nodes     = 3
+}
